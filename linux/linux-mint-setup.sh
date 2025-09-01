b39e5576b4c3761acdeaa7dffcf0dad693e0fe9a
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2025-09-01 13:31:53 +0700

    add linux mint setup file

---
+++ linux/linux-mint-setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This script not cover external packages like VsCode, Docker, etc.
+# Some thing should be config after run this script for some reasons.
+
+# fcitx not remember font size, here is how to fix:
+# Fictx Configuration > Global Config > Show Advandced Options > Program > Seconds sleep before fcitx really start
+
+# cinnamon_cursor_size=$(gsettings get org.cinnamon.desktop.interface cursor-size)
+
+# Fonts
+sudo apt install -y ttf-mscorefonts-installer \
+    fonts-jetbrains-mono \
+    fonts-inter
+
+# Command line tools
+sudo apt purge -y vim-tiny && sudo apt autoremove
+sudo apt install -y neovim \
+    fzf
+
+# GUI apps that doesn't have flatpak official Flatpak or just make sense to install system package
+sudo apt install -y audacity \
+    gparted
+
+# Flatpak apps setup
+flatpak install flathub -y org.gnome.Snapshot # Disable if there's no webcam
+flatpak install flathub -y org.gnome.clocks
+flatpak install flathub -y com.github.tchx84.Flatseal
+flatpak install flathub -y org.gimp.GIMP
+flatpak install flathub -y org.kde.kdenlive
+flatpak install flathub -y org.inkscape.Inkscape
+flatpak install flathub -y com.obsproject.Studio
+
+# Fix flatpak mouse inconsistent
+mkdir -p ~/.local/share/icons/default
+printf '[Icon Theme]
+Name=Bibata-Modern-Classic
+Inherits=Bibata-Modern-Classic' >> ~/.local/share/icons/default/index.theme
+
+# cinnamon_cursor_size * display_scale, should set in flatseal
+# flatpak override --env=XCURSOR_SIZE=48
